8c334afb3107b80bac4bd8057ba67d4f949eaf14
ivan0v42 <45047138+ivan0v42@users.noreply.github.com>
2018-11-29 22:57:39 +0300

    Update FirstTest.java

--- src/test/java/com/simbirsoft/qaa/course/FirstTest.java
+++ src/test/java/com/simbirsoft/qaa/course/FirstTest.java
@@ -25,7 +25,7 @@ public class FirstTest {
 
     @BeforeMethod
         public void beforeMethod() {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
         driver = new ChromeDriver();
         }
        @Test
